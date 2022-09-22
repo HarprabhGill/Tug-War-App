10e683e202161e0d45fc3dfb8b5d273152f1b7cf
HarprabhGill <60674278+HarprabhGill@users.noreply.github.com>
2022-09-22 16:06:56 -0600

    Added hand buttons and rope

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity style={styles.button} onPress={()=>{alert("you clicked me")}}>
+          <Image source={require("./assets/hand.png")} resizeMode='contain' style={{ height: 150, transform: [{ rotate: '180deg' }]}}/>
+      </TouchableOpacity>
+
+      <Image source={require("./assets/rope.png")} resizeMode='contain' style={{ height: 500, width: 400 }}/>
+      
+
+      <TouchableOpacity style={styles.button} onPress={()=>{alert("you clicked me 2")}}>
+          <Image source={require("./assets/hand.png")} resizeMode='contain' style={{ height: 150 }}/>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,6 +22,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center',  
+  },
+
+  button: {
+
   },
 });
