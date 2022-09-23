cb284c3b8af62c31e8a334de4745d331288ac9b1
HarprabhGill <60674278+HarprabhGill@users.noreply.github.com>
2022-09-23 15:07:45 -0600

    Rope knot moves based on which hand is being clicked

--- App.js
+++ App.js
@@ -1,33 +1,45 @@
 import { StyleSheet, Button, View, SafeAreaView, Text, Alert, Image, TouchableOpacity, ImageBackground, Animated } from 'react-native';
-export default function App() {
+import React, {Component} from 'react';
+export default class App extends Component {
 
-  const topclick = () => {
-    console.log('On click works')
+  constructor(props) {
+    super(props);
+    this.state = {
+      up: 0,
+    };
+    this.topclick = this.topclick.bind(this);
+    this.bottomclick = this.bottomclick.bind(this);
+  }
+
+  topclick = () => {
+    this.setState({up: this.state.up - 10});
   };
 
-  const bottomclick = () => {
-    console.log('On click works')
+  bottomclick = () => {
+    this.setState({up: this.state.up + 10});
   };
 
-  return (
-    <View style={styles.container}>
-      <ImageBackground source={require("./assets/grass.jpg")} resizeMode="cover" style={styles.container}>
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={require("./assets/grass.jpg")} resizeMode="cover" style={styles.container}>
 
-        <TouchableOpacity style={styles.button} onPress={topclick}>
-            <Image source={require("./assets/hand.png")} resizeMode='contain' style={{ height: 150, transform: [{ rotate: '180deg' }]}}/>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={this.topclick}>
+              <Image source={require("./assets/hand.png")} resizeMode='contain' style={{ height: 150, transform: [{ rotate: '180deg' }]}}/>
+          </TouchableOpacity>
 
-        <ImageBackground source={require("./assets/rope.png")} resizeMode='contain' style={styles.rope}>
-          <Image source={require("./assets/ropeKnot.png")} resizeMode='contain' style={styles.knot}/>
+          <ImageBackground source={require("./assets/rope.png")} resizeMode='contain' style={styles.rope}>
+            <Image source={require("./assets/ropeKnot.png")} resizeMode='contain' style={{top: this.state.up, height: 150, width:150}}/>
+          </ImageBackground>
+
+          <TouchableOpacity style={styles.button} onPress={this.bottomclick}>
+              <Image source={require("./assets/hand.png")} resizeMode='contain' style={{ height: 150 }}/>
+          </TouchableOpacity>
+
         </ImageBackground>
-
-        <TouchableOpacity style={styles.button} onPress={bottomclick}>
-            <Image source={require("./assets/hand.png")} resizeMode='contain' style={{ height: 150 }}/>
-        </TouchableOpacity>
-
-      </ImageBackground>
-    </View>
-  );
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -48,10 +60,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-
-  knot: {
-    height: 150, 
-    width: 150, 
-  },
-
 });
